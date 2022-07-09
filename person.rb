fbd945e3ee7ed4eb8379ffc67e2bb087eca3e457
Basit Arigbabuwo <arigbabuwobasitkorede6@gmail.com>
2022-07-09 12:07:20 +0100

    Add basic user interface

--- person.rb
+++ person.rb
@@ -1,33 +1,13 @@
-require_relative './nameable'
-require_relative './rental'
+class Rental
+  attr_accessor :date
+  attr_reader :book, :person
 
-class Person < Nameable
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = Random.rand(1..9999)
-    @name = name
-    @age = age
-    @parent_permission = parent_permission
-    super()
-  end
+  def initialize(date, book, person)
+    @date = date
+    @book = book
+    @person = person
 
-  attr_reader :id
-  attr_accessor :name, :age, :rental
-
-  def of_age?
-    @age >= 18
-  end
-
-  def can_use_services?
-    of_age?
-  end
-
-  private :of_age?
-
-  def correct_name
-    @name
-  end
-
-  def add_rental(book, date)
-    Rental.new(date, book, self)
+    person.rentals << self
+    book.rentals << self
   end
 end
