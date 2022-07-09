2a413072204e37042aa8b8667386b37af65417d9
Basit Arigbabuwo <arigbabuwobasitkorede6@gmail.com>
2022-07-09 12:20:37 +0100

    ix linter errors

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,12 +1,12 @@
 require './nameable'
 
-class CapitalizeDecorator < Nameable
+class TrimmerDecorator < Nameable
   def initialize(nameable = Nameable.new)
     super()
     @nameable = nameable
   end
 
   def correct_name
-    @nameable.correct_name.upcase
+    @nameable.correct_name.slice(0, 10)
   end
 end
